c929a1d1a4eefdd9e5550ec82396e1a27b4cf35d
Dean <deanjoneskingsley@artfx.fr>
2022-02-16 00:01:29 +0200

    Updates Matrix Multiplication

--- ParallelismAndConcurrency/HelloWorld.cpp
+++ ParallelismAndConcurrency/HelloWorld.cpp
@@ -9,12 +9,12 @@ void HelloWorld() {
 	cout << "'Hello World' ran from this: " << this_id << " thread\n" << endl;
 }
 
-int main() {
-	std::thread t1(HelloWorld);
-	std::thread t2(HelloWorld);
-
-	t1.join();
-	t2.join();
-
-	return 0;
-}
+//int main() {
+//	std::thread t1(HelloWorld);
+//	std::thread t2(HelloWorld);
+//
+//	t1.join();
+//	t2.join();
+//
+//	return 0;
+//}
--- ParallelismAndConcurrency/MatrixProduct.cpp
+++ ParallelismAndConcurrency/MatrixProduct.cpp
@@ -1,11 +1,69 @@
 #include <thread>
 #include <iostream>
+#include <vector>
+
+/* Create Matrix Class */
+class Matrix {
+public:
+	std::vector< std::vector<int> > elements;
+	std::size_t rows;
+	std::size_t cols;
+
+	Matrix()
+		: Matrix(0, 0) {}
+	Matrix(std::size_t rowsP, std::size_t colsP)
+		: elements(rowsP * colsP), rows(rowsP), cols(colsP) {}
+
+	std::size_t row_count() const { return rows; }
+	std::size_t col_count() const { return cols; }
+};
+
+/* Thread Multiplication */
+void multMatrixRow(Matrix a, Matrix b, Matrix sum, int index) {
+	for (int i = 0; i < b.cols; ++i) {
+		for (int j = 0; j < a.cols; ++j) {
+			sum.elements[index][i] += (a.elements[index][i] * b.elements[j][i]);
+		}
+	}
+}
+
+Matrix multMatrix(Matrix a, Matrix b) {
+	Matrix matrixSum;
+	if (a.rows == b.cols) {
+		std::vector<std::thread> threads;
+		matrixSum = Matrix(a.rows, b.cols);
+
+		/* Multiply Matrix Row */
+		for (int i = 0; i < matrixSum.rows; ++i) {
+			threads.emplace_back(multMatrixRow, a, b, matrixSum, i);
+		}
+		/* Wait for threads */
+		for (int i = 0; i < threads.size(); ++i) {
+			threads[i].join();
+		}
+	}
+	return matrixSum;
+}
+
+Matrix fillMatrix(Matrix matrix) {
+	for (int i = 0; i < matrix.cols; ++i) {
+		for (int j = 0; j < matrix.rows; ++j) {
+			matrix.elements[i][j] = i + j;
+		}
+	}
+}
+
 
 int main() 
 {
-	// matrices
-	int x[3][2] = { {0,1}, {2,3}, {4,5} };
-	int y[2][3] = { {6,7,8}, {9,10,11} };
+	/* Matrices */
+	//int matrix0[3][2] = { {0,1}, {2,3}, {4,5} };
+	//int matrix1[2][3] = { {6,7,8}, {9,10,11} };
+	//int matrixSum[3][3] = { {0,0,0}, {0,0,0}, {0,0,0} };
 
+	Matrix matrix0(3, 2);
+	Matrix matrix1(2, 3);
+	Matrix matrixResult(3, 3); 
 
+	matrix0 = fillMatrix(matrix0);
 }
