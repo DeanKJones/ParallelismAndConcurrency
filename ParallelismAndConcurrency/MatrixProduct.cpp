b15eed2503df5a0674e8864d3c23d8c985611392
Dean <deanjoneskingsley@artfx.fr>
2022-02-18 22:50:27 +0200

    Updates

--- ParallelismAndConcurrency/MatrixProduct.cpp
+++ ParallelismAndConcurrency/MatrixProduct.cpp
@@ -1,69 +1,72 @@
 #include <thread>
 #include <iostream>
 #include <vector>
+#include <string>
 
 /* Create Matrix Class */
 class Matrix {
 public:
-	std::vector< std::vector<int> > elements;
+	std::vector<int> elements;
 	std::size_t rows;
 	std::size_t cols;
 
-	Matrix()
-		: Matrix(0, 0) {}
+	Matrix() = delete;
+	~Matrix() {}
 	Matrix(std::size_t rowsP, std::size_t colsP)
 		: elements(rowsP * colsP), rows(rowsP), cols(colsP) {}
-
-	std::size_t row_count() const { return rows; }
-	std::size_t col_count() const { return cols; }
+	
+	int& operator()(std::size_t i, std::size_t j) {
+		return elements[i * cols + j];
+	}
 };
 
 /* Thread Multiplication */
-void multMatrixRow(Matrix a, Matrix b, Matrix sum, int index) {
+void multMatrixRow(Matrix a, Matrix b, Matrix& sum, int index) {
+
 	for (int i = 0; i < b.cols; ++i) {
-		for (int j = 0; j < a.cols; ++j) {
-			sum.elements[index][i] += (a.elements[index][i] * b.elements[j][i]);
+		for (int j = 0; j < a.rows; ++j) {
+			int indexI = index + i;
+			int indexJ = index + j;
+			int IJ = i + j;
+
+			// Incorrect math
+			sum.elements[indexI] += a.elements[indexJ] * b.elements[IJ];
 		}
 	}
+	std::cout << "Row: " << std::to_string(index) << " complete.\n";
 }
 
-Matrix multMatrix(Matrix a, Matrix b) {
-	Matrix matrixSum;
-	if (a.rows == b.cols) {
-		std::vector<std::thread> threads;
-		matrixSum = Matrix(a.rows, b.cols);
+Matrix multMatrix(Matrix m1, Matrix m2) {
 
-		/* Multiply Matrix Row */
-		for (int i = 0; i < a.rows; ++i) {
-			threads.emplace_back(multMatrixRow, a, b, matrixSum, i);
-		}
-		/* Wait for threads */
-		for (int i = 0; i < threads.size(); ++i) {
-			threads[i].join();
-		}
-	}
+	Matrix matrixSum(m1.rows, m2.cols);
+
+	std::thread t0(multMatrixRow, m1, m2, std::ref(matrixSum), 0);
+	std::thread t1(multMatrixRow, m1, m2, std::ref(matrixSum), 1);
+
+	t0.join();
+	t1.join();
+
 	return matrixSum;
 }
 
 Matrix fillMatrix(Matrix matrix) {
 	Matrix matrixLoop = matrix;
 	int k = 0;
-	for (int i = 0; i < matrixLoop.cols; ++i) {
-		for (int j = 0; j < matrixLoop.rows; ++j) {
-			matrixLoop.elements[j].push_back(k);
-			k += 1;
-		}
+
+	for (int i = 0; i < matrixLoop.elements.size(); ++i) {
+		matrixLoop.elements[i] = k;
+		k += 1;
 	}
 	return matrixLoop;
 }
 
 void outputMatrix(Matrix matrix) {
+
 	Matrix matrixLoop = matrix;
 	std::cout << "Matrix:" << std::endl;
-	for (int i = 0; i < matrixLoop.cols; ++i) {
-		for (int j = 0; j < matrixLoop.rows; ++j) {
-			std::cout << matrixLoop.elements[j][i] << " \n"[j == (matrixLoop.rows - 1)];
-		}
+
+	for (int i = 0; i < matrixLoop.elements.size(); ++i) {
+		std::cout << matrixLoop.elements[i] << " \n"[i == (matrixLoop.rows - 1)];
 	}
 	std::cout << "\n";
 }
@@ -72,20 +75,20 @@ void outputMatrix(Matrix matrix) {
 int main() 
 {
 	/* Matrices */
-	Matrix matrix0(3, 2);
-	Matrix matrix1(2, 3);
-	Matrix matrixResult(3, 3); 
+	Matrix matrix0(2, 2);
+	Matrix matrix1(2, 2);
+	Matrix matrixResult(2, 2); 
 
 	/* Fill Matrices */
 	matrix0 = fillMatrix(matrix0);
 	matrix1 = fillMatrix(matrix1);
 
+	outputMatrix(matrix0);
+	outputMatrix(matrix1);
+
 	/* Do Multiplication */
 	matrixResult = multMatrix(matrix0, matrix1);
-
 	/* Output */
-	outputMatrix(matrix0);
-	outputMatrix(matrix1); 
 	outputMatrix(matrixResult);
 
 	return 0;
